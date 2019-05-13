9f83ca4a7b22fdc09a7f4811ec8cf0d9129becd0
Josh Guerra <joshua.guerra21@gmail.com>
2019-05-13 13:34:30 -0500

    css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from "react";
 function Header() {
     return (
         <header>
-            <h1>(React) Google Books Search</h1>
-            <p>Search for and Save Books of Interest</p>
+            <p>Search and Save Books</p>
         </header>
     );
 }
